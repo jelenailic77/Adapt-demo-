271b289b8d6b783438066dddee36b259f6e21341
Jelena Ilic <jilic@transperfect.comm>
2024-07-16 16:28:18 +0200

    update lottie and background

--- src/extensions/adapt-blocklottieInteractivity/templates/lottieInteractivity.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { html, templates } from 'core/js/reactHelpers';
-
-export default function LottieInteractivity(props) {
-  const { lottieplayer} = props;
-    return <div dangerouslySetInnerHTML={{ __html: lottieplayer }} />
-   
- 
-}
